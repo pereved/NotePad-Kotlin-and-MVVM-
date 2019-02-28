c24e70fe815a7aef09fe5eaa1bdce81f86132d89
pereved <31728605+pereved@users.noreply.github.com>
2019-02-28 15:46:32 +0300

    First commit

---
+++ app/src/main/java/com/pereved/notepad/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pereved.notepad
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
